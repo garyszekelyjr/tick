1377e4c2ef922d2986e90deb555411193e13e5e4
Gary Szekely <gary.szekely.jr@gmail.com>
2025-02-27 06:04:12 -0500

    changes

---
+++ tests/test_trader.py
@@ -0,0 +1,66 @@
+import unittest
+
+from datetime import datetime
+
+from bargaineer import TZ
+from bargaineer.schwab import client
+
+
+class TestAccounts(unittest.TestCase):
+    def test_account_numbers(self):
+        response = client.request(client.ENDPOINTS["/accounts/accountNumbers"]())
+        self.assertEqual(response.status_code, 200)
+
+    def test_accounts(self):
+        response = client.request(client.ENDPOINTS["/accounts"]())
+        self.assertEqual(response.status_code, 200)
+
+    def test_account(self):
+        response = client.request(client.ENDPOINTS["/accounts/accountNumbers"]())
+        self.assertEqual(response.status_code, 200)
+        account_numbers = response.json()
+        for account_number in account_numbers:
+            response = client.request(
+                client.ENDPOINTS["/accounts/{accountNumber}"](
+                    account_number["hashValue"]
+                )
+            )
+            self.assertEqual(response.status_code, 200)
+
+
+class TestTransactions(unittest.TestCase):
+    def setUp(self) -> None:
+        response = client.request(client.ENDPOINTS["/accounts/accountNumbers"]())
+        self.assertEqual(response.status_code, 200)
+        self.account_numbers = response.json()
+
+    def test_transactions(self):
+        start_date = datetime(2025, 1, 1, 0, 0, 0, 0, TZ)
+        end_date = datetime(2025, 12, 31, 23, 59, 59, 999999, TZ)
+        for account_number in self.account_numbers:
+            response = client.request(
+                **client.ENDPOINTS["/accounts/{accountNumber}/transactions"](
+                    account_number["hashValue"], start_date, end_date
+                )
+            )
+            self.assertEqual(response.status_code, 200)
+
+    def test_transaction(self):
+        start_date = datetime(2025, 1, 1, 0, 0, 0, 0, TZ)
+        end_date = datetime(2025, 12, 31, 23, 59, 59, 999999, TZ)
+        for account_number in self.account_numbers:
+            response = client.request(
+                **client.ENDPOINTS["/accounts/{accountNumber}/transactions"](
+                    account_number["hashValue"], start_date, end_date
+                )
+            )
+            self.assertEqual(response.status_code, 200)
+            transactions = response.json()
+            transaction = transactions[0]
+            if transaction:
+                response = client.request(
+                    client.ENDPOINTS[
+                        "/accounts/{accountNumber}/transactions/{transactionId}"
+                    ](account_number["hashValue"], transaction["activityId"])
+                )
+                self.assertEqual(response.status_code, 200)
